8dfb1320d74e84a31312fd285a5481a3ef8c6832
Jonsama <jonsama@am-0800271aee48.intern>
2024-09-20 12:51:41 +0200

    @feat: Implementació de la funció de resta

--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,4 @@
 #ifndef CALC_H
 #define CALC_H
-
-
+double resta(double a, double b);
 #endif // CALC_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include "calc.h"  
+#include "calc.h"
 
 void mostrar_ajuda() {
     printf("Ús: calc [OPCIONS]\n");
@@ -55,8 +55,8 @@ int main(int argc, char *argv[]) {
                 if (argc >= optind + 1) {
                     num1 = atof(optarg);
                     num2 = atof(argv[optind]);
-                    //printf("Resultat de la resta: %.2f\n", resta(num1, num2));
-                    printf("Not implemented yet!\n");
+                    printf("Resultat de la resta: %.2f\n", resta(num1, num2));
+                    //printf("Not implemented yet!\n");
                 } else {
                     printf("Falten arguments per a la resta.\n");
                     return 1;
