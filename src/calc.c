9210561ec20e3a8346af283db3e5c6fec6a22799
seac <seac@debian.local>
2024-09-20 13:04:19 +0200

    @feat: Implementació de la funció division

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+double divisio(double a, double b){
+    return a / b ;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
 
-
+double divisio(double a, double b);
 #endif // CALC_H
